2f0085a189a487e5f82ce4cf32e3f290f921020b
Kuthejul Kubra <kuthejul@Services-MacBook-Pro.local>
2024-11-14 10:37:40 +0530

    Initial react and tailwind css setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div className='text-3xl text-blue-700'>
+    hello world 
+   </div>
+      
+  )
+}
+
+export default App
