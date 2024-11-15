d5c08c459110b74780402f83c46b7957895f55ac
Kuthejul Kubra <kuthejul@Services-MacBook-Pro.local>
2024-11-15 12:35:56 +0530

    Added changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Table from './components/table'
+import Table from './components/Table.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
