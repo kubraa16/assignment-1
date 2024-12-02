6af84b5970a2f083b481a7a43ba2501004de64d7
Kuthejul Kubra <kuthejul@Services-MacBook-Pro.local>
2024-12-02 10:39:06 +0530

    Made changes in Customcell component

--- src/components/core/CustomCell.jsx
+++ src/components/core/CustomCell.jsx
@@ -1,20 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const CustomCell = ({ data, isTicker }) => {
+const CustomCell = ({ data }) => {
   return (
-    <td className="px-4 py-2 border border-gray-300 truncate w-1/3">
-      {isTicker ? (
-        <Link
-          to={`/company/${data}`}
-          className="text-blue-500 hover:text-blue-700"
-        >
-          {data}
-        </Link>
-      ) : (
-        data
-      )}
-    </td>
+    <td className="px-4 py-2 border border-gray-300 truncate w-1/3">{data}</td>
   );
 };
 
