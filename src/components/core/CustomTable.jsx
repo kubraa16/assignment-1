adfe1ef1a4b821fa4007c5b095b249e175780186
Kuthejul Kubra <kuthejul@Services-MacBook-Pro.local>
2024-12-02 18:37:56 +0530

    bug fixes

--- src/components/core/CustomTable.jsx
+++ src/components/core/CustomTable.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import CustomRow from "./CustomRow";
 import CustomHeader from "./CustomHeader";
 const CustomTable = ({ data, header }) => {
--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import CustomTable from "../core/CustomTable";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -8,6 +9,7 @@ import {
   losersSelecter,
 } from "../../store/selectors/stocksSelector";
 import { fetchStocksData } from "../../store/reducers/stocksSlice";
+import { headers } from "../../data/data.json";
 
 const IndexTable = () => {
   const dispatch = useDispatch();
@@ -26,12 +28,27 @@ const IndexTable = () => {
   if (error) {
     return <h1>Error: {error}</h1>;
   }
+
+  for (let i = 0; i < headers.length; i += 1) {
+    if (headers[i].key == "ticker") {
+      headers[i].render = (key, data) => (
+        <Link
+          to={`/company/${key}`}
+          className="text-blue-500 hover:text-blue-700"
+        >
+          {key}
+        </Link>
+      );
+      break;
+    }
+  }
+
   return (
     <div>
       <h2 className="text-lg font-bold py-3">Top Gainers</h2>
-      <CustomTable data={topGainers} />
+      <CustomTable data={topGainers} header={headers} />
       <h2 className="text-lg font-bold py-3">Top Loosers</h2>
-      <CustomTable data={topLosers} />
+      <CustomTable data={topLosers} header={headers} />
     </div>
   );
 };
